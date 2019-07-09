a7cc582875123006aa676a9570abb949542f9b15
CaraBrookins <cjbrookins438@gmail.com>
2019-07-09 10:48:21 -0500

    added instructor bean and insertInstructor servlet

--- encuestas/src/main/java/ws/synopsis/surveys/servlets/InsertInstructor.java
+++ encuestas/src/main/java/ws/synopsis/surveys/servlets/InsertInstructor.java
@@ -5,9 +5,11 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-import ws.synopsis.surveys.model.Estudiante;
-import ws.synopsis.surveys.utils.EstudianteDB;
+import ws.synopsis.surveys.model.Instructor;
+import ws.synopsis.surveys.utils.HashingUtil;
+import ws.synopsis.surveys.utils.InstructorDB;
 
 /**
  * Servlet implementation class InsertInstructor
@@ -24,19 +26,29 @@ public class InsertInstructor extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		System.out.println("/InsertInstructor.java");
 		
-		String DNI = request.getParameter("DNI");
-		String userType = request.getParameter("userType");
-		String nombre = request.getParameter("nombre");
-		String apellido = request.getParameter("apellido");
-		String userName = request.getParameter("userName");
-		String contrasena = request.getParameter("contrasena");
-		String correo = request.getParameter("correo");
-		String telefono = request.getParameter("telefono");
-		String compania = request.getParameter("compania");
-		String cargo = request.getParameter("cargo");
-		Estudiante estudiante = new Estudiante(DNI, userType, nombre, apellido, userName, contrasena, correo, telefono, compania, cargo);
-		EstudianteDB.insertEstudiante(estudiante);
+		Instructor instructor = new Instructor();
+		
+		instructor.setDNI(request.getParameter("DNI"));
+		instructor.setUserType(request.getParameter("userType"));
+		instructor.setNombre(request.getParameter("nombre"));
+		instructor.setApellido(request.getParameter("apellido"));
+		instructor.setUsername(request.getParameter("userName"));
+		instructor.setContrasena(HashingUtil.shaw256((request.getParameter("contrasena"))));
+		instructor.setCorreo(request.getParameter("correo"));
+		instructor.setTelefono(request.getParameter("telefono"));
+		
+		InstructorDB.insertInstructor(instructor);
+		
+		HttpSession session = request.getSession(true);
+		System.out.println(session);
+		
+		session.setAttribute("username", request.getParameter("userName"));
+		session.setAttribute("instructor", instructor);
+	
+		response.sendRedirect("/encuestas/Dash.html");
+		
 	}
 
 
